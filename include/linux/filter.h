a6578fbc123d3aaff09a8a7b2e83baeb0c93d51b
Eduard Zingerman <eddyz87@gmail.com>
2024-12-09 20:10:54 -0800

    bpf: refactor bpf_helper_changes_pkt_data to use helper number
    BugLink: https://bugs.launchpad.net/bugs/2106632
    
    [ Upstream commit b238e187b4a2d3b54d80aec05a9cab6466b79dde ]
    
    Use BPF helper number instead of function pointer in
    bpf_helper_changes_pkt_data(). This would simplify usage of this
    function in verifier.c:check_cfg() (in a follow-up patch),
    where only helper number is easily available and there is no real need
    to lookup helper proto.
    
    Signed-off-by: Eduard Zingerman <eddyz87@gmail.com>
    Link: https://lore.kernel.org/r/20241210041100.1898468-3-eddyz87@gmail.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Stable-dep-of: 1a4607ffba35 ("bpf: consider that tail calls invalidate packet pointers")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Manuel Diewald <manuel.diewald@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- include/linux/filter.h
+++ include/linux/filter.h
@@ -961,7 +961,7 @@ bool bpf_jit_supports_kfunc_call(void);
 bool bpf_jit_supports_far_kfunc_call(void);
 bool bpf_jit_supports_exceptions(void);
 void arch_bpf_stack_walk(bool (*consume_fn)(void *cookie, u64 ip, u64 sp, u64 bp), void *cookie);
-bool bpf_helper_changes_pkt_data(void *func);
+bool bpf_helper_changes_pkt_data(enum bpf_func_id func_id);
 
 static inline bool bpf_dump_raw_ok(const struct cred *cred)
 {
