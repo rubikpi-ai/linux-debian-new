f2249a4e2ed50b493058936e0d76d0acddfb5f30
Mike Snitzer <snitzer@kernel.org>
2025-02-24 21:20:02 -0500

    NFS: fix nfs_release_folio() to not deadlock via kcompactd writeback
    BugLink: https://bugs.launchpad.net/bugs/2117533
    
    commit ce6d9c1c2b5cc785016faa11b48b6cd317eb367e upstream.
    
    Add PF_KCOMPACTD flag and current_is_kcompactd() helper to check for it so
    nfs_release_folio() can skip calling nfs_wb_folio() from kcompactd.
    
    Otherwise NFS can deadlock waiting for kcompactd enduced writeback which
    recurses back to NFS (which triggers writeback to NFSD via NFS loopback
    mount on the same host, NFSD blocks waiting for XFS's call to
    __filemap_get_folio):
    
    6070.550357] INFO: task kcompactd0:58 blocked for more than 4435 seconds.
    
    {---
    [58] "kcompactd0"
    [<0>] folio_wait_bit+0xe8/0x200
    [<0>] folio_wait_writeback+0x2b/0x80
    [<0>] nfs_wb_folio+0x80/0x1b0 [nfs]
    [<0>] nfs_release_folio+0x68/0x130 [nfs]
    [<0>] split_huge_page_to_list_to_order+0x362/0x840
    [<0>] migrate_pages_batch+0x43d/0xb90
    [<0>] migrate_pages_sync+0x9a/0x240
    [<0>] migrate_pages+0x93c/0x9f0
    [<0>] compact_zone+0x8e2/0x1030
    [<0>] compact_node+0xdb/0x120
    [<0>] kcompactd+0x121/0x2e0
    [<0>] kthread+0xcf/0x100
    [<0>] ret_from_fork+0x31/0x40
    [<0>] ret_from_fork_asm+0x1a/0x30
    ---}
    
    [akpm@linux-foundation.org: fix build]
    Link: https://lkml.kernel.org/r/20250225022002.26141-1-snitzer@kernel.org
    Fixes: 96780ca55e3c ("NFS: fix up nfs_release_folio() to try to release the page")
    Signed-off-by: Mike Snitzer <snitzer@kernel.org>
    Cc: Anna Schumaker <anna.schumaker@oracle.com>
    Cc: Trond Myklebust <trond.myklebust@hammerspace.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Noah Wager <noah.wager@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- mm/compaction.c
+++ mm/compaction.c
@@ -3086,6 +3086,7 @@ static int kcompactd(void *p)
 	if (!cpumask_empty(cpumask))
 		set_cpus_allowed_ptr(tsk, cpumask);
 
+	current->flags |= PF_KCOMPACTD;
 	set_freezable();
 
 	pgdat->kcompactd_max_order = 0;
@@ -3141,6 +3142,8 @@ static int kcompactd(void *p)
 		if (unlikely(pgdat->proactive_compact_trigger))
 			pgdat->proactive_compact_trigger = false;
 	}
+
+	current->flags &= ~PF_KCOMPACTD;
 
 	return 0;
 }
