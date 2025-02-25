5f606e928ab08be1825bee3c99cb84f780c2e6e7
Vlastimil Babka <vbabka@suse.cz>
2025-02-25 22:59:22 +0900

    mm, mmap: limit THP alignment of anonymous mappings to PMD-aligned sizes
    BugLink: https://bugs.launchpad.net/bugs/2099996
    
    [ Upstream commit d4148aeab412432bf928f311eca8a2ba52bb05df ]
    
    Since commit efa7df3e3bb5 ("mm: align larger anonymous mappings on THP
    boundaries") a mmap() of anonymous memory without a specific address hint
    and of at least PMD_SIZE will be aligned to PMD so that it can benefit
    from a THP backing page.
    
    However this change has been shown to regress some workloads
    significantly.  [1] reports regressions in various spec benchmarks, with
    up to 600% slowdown of the cactusBSSN benchmark on some platforms.  The
    benchmark seems to create many mappings of 4632kB, which would have merged
    to a large THP-backed area before commit efa7df3e3bb5 and now they are
    fragmented to multiple areas each aligned to PMD boundary with gaps
    between.  The regression then seems to be caused mainly due to the
    benchmark's memory access pattern suffering from TLB or cache aliasing due
    to the aligned boundaries of the individual areas.
    
    Another known regression bisected to commit efa7df3e3bb5 is darktable [2]
    [3] and early testing suggests this patch fixes the regression there as
    well.
    
    To fix the regression but still try to benefit from THP-friendly anonymous
    mapping alignment, add a condition that the size of the mapping must be a
    multiple of PMD size instead of at least PMD size.  In case of many
    odd-sized mapping like the cactusBSSN creates, those will stop being
    aligned and with gaps between, and instead naturally merge again.
    
    Link: https://lkml.kernel.org/r/20241024151228.101841-2-vbabka@suse.cz
    Fixes: efa7df3e3bb5 ("mm: align larger anonymous mappings on THP boundaries")
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>
    Reported-by: Michael Matz <matz@suse.de>
    Debugged-by: Gabriel Krisman Bertazi <gabriel@krisman.be>
    Closes: https://bugzilla.suse.com/show_bug.cgi?id=1229012 [1]
    Reported-by: Matthias Bodenbinder <matthias@bodenbinder.de>
    Closes: https://bugzilla.kernel.org/show_bug.cgi?id=219366 [2]
    Closes: https://lore.kernel.org/all/2050f0d4-57b0-481d-bab8-05e8d48fed0c@leemhuis.info/ [3]
    Reviewed-by: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Reviewed-by: Yang Shi <yang@os.amperecomputing.com>
    Cc: Rik van Riel <riel@surriel.com>
    Cc: Jann Horn <jannh@google.com>
    Cc: Liam R. Howlett <Liam.Howlett@Oracle.com>
    Cc: Petr Tesarik <ptesarik@suse.com>
    Cc: Thorsten Leemhuis <regressions@leemhuis.info>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    [koichiroden: adjusted context due to missing patch series:
    "Cover a guard gap corner case" by Rick Edgecombe]
    Signed-off-by: Koichiro Den <koichiro.den@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -1835,7 +1835,8 @@ get_unmapped_area(struct file *file, unsigned long addr, unsigned long len,
 		 * so use shmem's get_unmapped_area in case it can be huge.
 		 */
 		get_area = shmem_get_unmapped_area;
-	} else if (IS_ENABLED(CONFIG_TRANSPARENT_HUGEPAGE)) {
+	} else if (IS_ENABLED(CONFIG_TRANSPARENT_HUGEPAGE)
+		   && IS_ALIGNED(len, PMD_SIZE)) {
 		/* Ensures that larger anonymous mappings are THP aligned. */
 		get_area = thp_get_unmapped_area;
 	}
