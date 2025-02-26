edff5b64970f1c9338be24a9c2e10dc20193099d
Nirmoy Das <nirmoy.das@intel.com>
2025-02-26 22:49:14 +0900

    drm/xe: Move LNL scheduling WA to xe_device.h
    BugLink: https://bugs.launchpad.net/bugs/2100292
    
    [ Upstream commit 55e8a3f37e54eb1c7b914d6d5565a37282ec1978 ]
    
    Move LNL scheduling WA to xe_device.h so this can be used in other
    places without needing keep the same comment about removal of this WA
    in the future. The WA, which flushes work or workqueues, is now wrapped
    in macros and can be reused wherever needed.
    
    Cc: Badal Nilawar <badal.nilawar@intel.com>
    Cc: Matthew Auld <matthew.auld@intel.com>
    Cc: Matthew Brost <matthew.brost@intel.com>
    Cc: Himal Prasad Ghimiray <himal.prasad.ghimiray@intel.com>
    Cc: Lucas De Marchi <lucas.demarchi@intel.com>
    cc: stable@vger.kernel.org # v6.11+
    Suggested-by: John Harrison <John.C.Harrison@Intel.com>
    Signed-off-by: Nirmoy Das <nirmoy.das@intel.com>
    Reviewed-by: Matthew Auld <matthew.auld@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20241029120117.449694-1-nirmoy.das@intel.com
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>
    (cherry picked from commit cbe006a6492c01a0058912ae15d473f4c149896c)
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Koichiro Den <koichiro.den@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- drivers/gpu/drm/xe/xe_device.h
+++ drivers/gpu/drm/xe/xe_device.h
@@ -166,4 +166,18 @@ static inline bool xe_device_has_sriov(struct xe_device *xe)
 
 u32 xe_device_ccs_bytes(struct xe_device *xe, u64 size);
 
+/*
+ * Occasionally it is seen that the G2H worker starts running after a delay of more than
+ * a second even after being queued and activated by the Linux workqueue subsystem. This
+ * leads to G2H timeout error. The root cause of issue lies with scheduling latency of
+ * Lunarlake Hybrid CPU. Issue disappears if we disable Lunarlake atom cores from BIOS
+ * and this is beyond xe kmd.
+ *
+ * TODO: Drop this change once workqueue scheduling delay issue is fixed on LNL Hybrid CPU.
+ */
+#define LNL_FLUSH_WORKQUEUE(wq__) \
+	flush_workqueue(wq__)
+#define LNL_FLUSH_WORK(wrk__) \
+	flush_work(wrk__)
+
 #endif
