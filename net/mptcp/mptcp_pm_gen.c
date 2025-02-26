c2e04891534326150b775a8be23119c1cb116ac3
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2025-02-26 22:49:14 +0900

    mptcp: no admin perm to list endpoints
    BugLink: https://bugs.launchpad.net/bugs/2100292
    
    commit cfbbd4859882a5469f6f4945937a074ee78c4b46 upstream.
    
    During the switch to YNL, the command to list all endpoints has been
    accidentally restricted to users with admin permissions.
    
    It looks like there are no reasons to have this restriction which makes
    it harder for a user to quickly check if the endpoint list has been
    correctly populated by an automated tool. Best to go back to the
    previous behaviour then.
    
    mptcp_pm_gen.c has been modified using ynl-gen-c.py:
    
       $ ./tools/net/ynl/ynl-gen-c.py --mode kernel \
         --spec Documentation/netlink/specs/mptcp_pm.yaml --source \
         -o net/mptcp/mptcp_pm_gen.c
    
    The header file doesn't need to be regenerated.
    
    Fixes: 1d0507f46843 ("net: mptcp: convert netlink from small_ops to ops")
    Cc: stable@vger.kernel.org
    Reviewed-by: Davide Caratti <dcaratti@redhat.com>
    Reviewed-by: Mat Martineau <martineau@kernel.org>
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>
    Link: https://patch.msgid.link/20241104-net-mptcp-misc-6-12-v1-1-c13f2ff1656f@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Koichiro Den <koichiro.den@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- net/mptcp/mptcp_pm_gen.c
+++ net/mptcp/mptcp_pm_gen.c
@@ -150,7 +150,6 @@ const struct genl_ops mptcp_pm_nl_ops[11] = {
 		.doit		= mptcp_pm_nl_announce_doit,
 		.policy		= mptcp_pm_announce_nl_policy,
 		.maxattr	= MPTCP_PM_ATTR_TOKEN,
-		.flags		= GENL_UNS_ADMIN_PERM,
 	},
 	{
 		.cmd		= MPTCP_PM_CMD_REMOVE,
