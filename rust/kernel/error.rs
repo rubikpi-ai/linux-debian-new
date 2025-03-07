2641c12d2d876ddbd1dd2b19a0e8bdca6096e190
Filipe Xavier <felipe_life@live.com>
2025-03-07 23:49:25 +0100

    rust: error: make conversion functions public
    BugLink: https://bugs.launchpad.net/bugs/2117533
    
    commit 5ed147473458f8c20f908a03227d8f5bb3cb8f7d upstream.
    
    Change visibility to public of functions in error.rs:
    from_err_ptr, from_errno, from_result and to_ptr.
    Additionally, remove dead_code annotations.
    
    Link: https://github.com/Rust-for-Linux/linux/issues/1105
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Signed-off-by: Filipe Xavier <felipe_life@live.com>
    Reviewed-by: Benno Lossin <benno.lossin@proton.me>
    Reviewed-by: Gary Guo <gary@garyguo.net>
    Link: https://lore.kernel.org/r/DM4PR14MB7276E6948E67B3B23D8EA847E9652@DM4PR14MB7276.namprd14.prod.outlook.com
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    [nwager: Context changes due to missing commits:
    7bc186731e87 ("rust: error: allow `useless_conversion` for 32-bit builds")
    706d4296b843 ("rust: start using the `#[expect(...)]` attribute")]
    Signed-off-by: Noah Wager <noah.wager@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- rust/kernel/error.rs
+++ rust/kernel/error.rs
@@ -99,7 +99,7 @@ impl Error {
     ///
     /// It is a bug to pass an out-of-range `errno`. `EINVAL` would
     /// be returned in such a case.
-    pub(crate) fn from_errno(errno: core::ffi::c_int) -> Error {
+    pub fn from_errno(errno: core::ffi::c_int) -> Error {
         if errno < -(bindings::MAX_ERRNO as i32) || errno >= 0 {
             // TODO: Make it a `WARN_ONCE` once available.
             crate::pr_warn!(
@@ -131,8 +131,8 @@ impl Error {
     }
 
     /// Returns the error encoded as a pointer.
-    #[allow(dead_code)]
-    pub(crate) fn to_ptr<T>(self) -> *mut T {
+    pub fn to_ptr<T>(self) -> *mut T {
+        #[cfg_attr(target_pointer_width = "32", allow(clippy::useless_conversion))]
         // SAFETY: `self.0` is a valid error due to its invariant.
         unsafe { bindings::ERR_PTR(self.0.into()) as *mut _ }
     }
@@ -273,9 +273,7 @@ pub fn to_result(err: core::ffi::c_int) -> Result {
 ///     }
 /// }
 /// ```
-// TODO: Remove `dead_code` marker once an in-kernel client is available.
-#[allow(dead_code)]
-pub(crate) fn from_err_ptr<T>(ptr: *mut T) -> Result<*mut T> {
+pub fn from_err_ptr<T>(ptr: *mut T) -> Result<*mut T> {
     // CAST: Casting a pointer to `*const core::ffi::c_void` is always valid.
     let const_ptr: *const core::ffi::c_void = ptr.cast();
     // SAFETY: The FFI function does not deref the pointer.
@@ -320,9 +318,7 @@ pub(crate) fn from_err_ptr<T>(ptr: *mut T) -> Result<*mut T> {
 ///     })
 /// }
 /// ```
-// TODO: Remove `dead_code` marker once an in-kernel client is available.
-#[allow(dead_code)]
-pub(crate) fn from_result<T, F>(f: F) -> T
+pub fn from_result<T, F>(f: F) -> T
 where
     T: From<i16>,
     F: FnOnce() -> Result<T>,
