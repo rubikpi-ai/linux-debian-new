05384fa21c575c441170056e09644408f7fd63f9
Konstantin Shkolnyy <kshk@linux.ibm.com>
2025-03-12 22:51:00 +0900

    vsock/test: fix parameter types in SO_VM_SOCKETS_* calls
    BugLink: https://bugs.launchpad.net/bugs/2102118
    
    [ Upstream commit 3f36ee29e732b68044d531f47d31f22d265954c6 ]
    
    Change parameters of SO_VM_SOCKETS_* to unsigned long long as documented
    in the vm_sockets.h, because the corresponding kernel code requires them
    to be at least 64-bit, no matter what architecture. Otherwise they are
    too small on 32-bit machines.
    
    Fixes: 5c338112e48a ("test/vsock: rework message bounds test")
    Fixes: 685a21c314a8 ("test/vsock: add big message test")
    Fixes: 542e893fbadc ("vsock/test: two tests to check credit update logic")
    Fixes: 8abbffd27ced ("test/vsock: vsock_perf utility")
    Signed-off-by: Konstantin Shkolnyy <kshk@linux.ibm.com>
    Reviewed-by: Stefano Garzarella <sgarzare@redhat.com>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Koichiro Den <koichiro.den@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- tools/testing/vsock/vsock_test.c
+++ tools/testing/vsock/vsock_test.c
@@ -427,7 +427,7 @@ static void test_seqpacket_msg_bounds_client(const struct test_opts *opts)
 
 static void test_seqpacket_msg_bounds_server(const struct test_opts *opts)
 {
-	unsigned long sock_buf_size;
+	unsigned long long sock_buf_size;
 	unsigned long remote_hash;
 	unsigned long curr_hash;
 	int fd;
@@ -632,7 +632,8 @@ static void test_seqpacket_timeout_server(const struct test_opts *opts)
 
 static void test_seqpacket_bigmsg_client(const struct test_opts *opts)
 {
-	unsigned long sock_buf_size;
+	unsigned long long sock_buf_size;
+	size_t buf_size;
 	socklen_t len;
 	void *data;
 	int fd;
@@ -653,13 +654,20 @@ static void test_seqpacket_bigmsg_client(const struct test_opts *opts)
 
 	sock_buf_size++;
 
-	data = malloc(sock_buf_size);
+	/* size_t can be < unsigned long long */
+	buf_size = (size_t)sock_buf_size;
+	if (buf_size != sock_buf_size) {
+		fprintf(stderr, "Returned BUFFER_SIZE too large\n");
+		exit(EXIT_FAILURE);
+	}
+
+	data = malloc(buf_size);
 	if (!data) {
 		perror("malloc");
 		exit(EXIT_FAILURE);
 	}
 
-	send_buf(fd, data, sock_buf_size, 0, -EMSGSIZE);
+	send_buf(fd, data, buf_size, 0, -EMSGSIZE);
 
 	control_writeln("CLISENT");
 
@@ -1279,6 +1287,7 @@ static void test_stream_credit_update_test(const struct test_opts *opts,
 	int recv_buf_size;
 	struct pollfd fds;
 	size_t buf_size;
+	unsigned long long sock_buf_size;
 	void *buf;
 	int fd;
 
@@ -1290,8 +1299,11 @@ static void test_stream_credit_update_test(const struct test_opts *opts,
 
 	buf_size = RCVLOWAT_CREDIT_UPD_BUF_SIZE;
 
+	/* size_t can be < unsigned long long */
+	sock_buf_size = buf_size;
+
 	if (setsockopt(fd, AF_VSOCK, SO_VM_SOCKETS_BUFFER_SIZE,
-		       &buf_size, sizeof(buf_size))) {
+		       &sock_buf_size, sizeof(sock_buf_size))) {
 		perror("setsockopt(SO_VM_SOCKETS_BUFFER_SIZE)");
 		exit(EXIT_FAILURE);
 	}
