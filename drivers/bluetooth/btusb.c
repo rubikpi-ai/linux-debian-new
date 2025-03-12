5be34705501474886dd1ec340a2745df3b02254a
Jiande Lu <jiande.lu@mediatek.com>
2025-03-12 22:51:00 +0900

    Bluetooth: btusb: Add 3 HWIDs for MT7925
    BugLink: https://bugs.launchpad.net/bugs/2102118
    
    [ Upstream commit de7dcf9d1df4b0009735756d0a2adff09c3f21d4 ]
    
    Add below HWIDs for MediaTek MT7925 USB Bluetooth chip.
    VID 0x0489, PID 0xe14f
    VID 0x0489, PID 0xe150
    VID 0x0489, PID 0xe151
    
    Patch has been tested successfully and controller is recognized
    device pair successfully.
    
    MT7925 module bring up message as below.
    Bluetooth: Core ver 2.22
    Bluetooth: HCI device and connection manager initialized
    Bluetooth: HCI socket layer initialized
    Bluetooth: L2CAP socket layer initialized
    Bluetooth: SCO socket layer initialized
    Bluetooth: hci0: HW/SW Version: 0x00000000, Build Time: 20240816133202
    Bluetooth: hci0: Device setup in 286558 usecs
    Bluetooth: hci0: HCI Enhanced Setup Synchronous Connection command is advertised, but not supported.
    Bluetooth: hci0: AOSP extensions version v1.00
    Bluetooth: BNEP (Ethernet Emulation) ver 1.3
    Bluetooth: BNEP filters: protocol multicast
    Bluetooth: BNEP socket layer initialized
    Bluetooth: MGMT ver 1.22
    Bluetooth: RFCOMM TTY layer initialized
    Bluetooth: RFCOMM socket layer initialized
    Bluetooth: RFCOMM ver 1.11
    
    Signed-off-by: Jiande Lu <jiande.lu@mediatek.com>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    [koichiroden: set BTUSB_VALID_LE_STATES flag. Commit 0fec656d08aa ("Bluetooth:
    btusb: Invert LE State flag to set invalid rather then valid") is missing.
    Also adjusted minor context conflicts]
    Signed-off-by: Koichiro Den <koichiro.den@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -700,6 +700,15 @@ static const struct usb_device_id quirks_table[] = {
 	{ USB_DEVICE(0x0489, 0xe124), .driver_info = BTUSB_MEDIATEK |
 						     BTUSB_WIDEBAND_SPEECH |
 						     BTUSB_VALID_LE_STATES },
+	{ USB_DEVICE(0x0489, 0xe14f), .driver_info = BTUSB_MEDIATEK |
+						     BTUSB_WIDEBAND_SPEECH |
+						     BTUSB_VALID_LE_STATES },
+	{ USB_DEVICE(0x0489, 0xe150), .driver_info = BTUSB_MEDIATEK |
+						     BTUSB_WIDEBAND_SPEECH |
+						     BTUSB_VALID_LE_STATES },
+	{ USB_DEVICE(0x0489, 0xe151), .driver_info = BTUSB_MEDIATEK |
+						     BTUSB_WIDEBAND_SPEECH |
+						     BTUSB_VALID_LE_STATES },
 	{ USB_DEVICE(0x13d3, 0x3602), .driver_info = BTUSB_MEDIATEK |
 						     BTUSB_WIDEBAND_SPEECH |
 						     BTUSB_VALID_LE_STATES },
