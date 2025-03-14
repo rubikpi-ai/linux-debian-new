1171ed4ef746a4f13c6e09ab12457b7ec7777c8f
Haibo Chen <haibo.chen@nxp.com>
2025-03-14 19:01:44 +0800

    can: flexcan: only change CAN state when link up in system PM
    BugLink: https://bugs.launchpad.net/bugs/2119458
    
    commit fd99d6ed20234b83d65b9c5417794343577cf3e5 upstream.
    
    After a suspend/resume cycle on a down interface, it will come up as
    ERROR-ACTIVE.
    
    $ ip -details -s -s a s dev flexcan0
    3: flexcan0: <NOARP,ECHO> mtu 16 qdisc pfifo_fast state DOWN group default qlen 10
        link/can  promiscuity 0 allmulti 0 minmtu 0 maxmtu 0
        can state STOPPED (berr-counter tx 0 rx 0) restart-ms 1000
    
    $ sudo systemctl suspend
    
    $ ip -details -s -s a s dev flexcan0
    3: flexcan0: <NOARP,ECHO> mtu 16 qdisc pfifo_fast state DOWN group default qlen 10
        link/can  promiscuity 0 allmulti 0 minmtu 0 maxmtu 0
        can state ERROR-ACTIVE (berr-counter tx 0 rx 0) restart-ms 1000
    
    And only set CAN state to CAN_STATE_ERROR_ACTIVE when resume process
    has no issue, otherwise keep in CAN_STATE_SLEEPING as suspend did.
    
    Fixes: 4de349e786a3 ("can: flexcan: fix resume function")
    Cc: stable@vger.kernel.org
    Signed-off-by: Haibo Chen <haibo.chen@nxp.com>
    Link: https://patch.msgid.link/20250314110145.899179-1-haibo.chen@nxp.com
    Reported-by: Marc Kleine-Budde <mkl@pengutronix.de>
    Closes: https://lore.kernel.org/all/20250314-married-polar-elephant-b15594-mkl@pengutronix.de
    [mkl: add newlines]
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Noah Wager <noah.wager@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- drivers/net/can/flexcan/flexcan-core.c
+++ drivers/net/can/flexcan/flexcan-core.c
@@ -2243,8 +2243,9 @@ static int __maybe_unused flexcan_suspend(struct device *device)
 		}
 		netif_stop_queue(dev);
 		netif_device_detach(dev);
+
+		priv->can.state = CAN_STATE_SLEEPING;
 	}
-	priv->can.state = CAN_STATE_SLEEPING;
 
 	return 0;
 }
@@ -2255,7 +2256,6 @@ static int __maybe_unused flexcan_resume(struct device *device)
 	struct flexcan_priv *priv = netdev_priv(dev);
 	int err;
 
-	priv->can.state = CAN_STATE_ERROR_ACTIVE;
 	if (netif_running(dev)) {
 		netif_device_attach(dev);
 		netif_start_queue(dev);
@@ -2275,6 +2275,8 @@ static int __maybe_unused flexcan_resume(struct device *device)
 
 			flexcan_chip_interrupts_enable(dev);
 		}
+
+		priv->can.state = CAN_STATE_ERROR_ACTIVE;
 	}
 
 	return 0;
